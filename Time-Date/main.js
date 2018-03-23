51c6a8c3022114d84f1ebbeea384fe6fee0b4378
2Shrug <george.vlachakis1@gmail.com>
2018-03-23 23:54:23 +0200

    Removed the If date function and replaced it with an array

--- Time-Date/main.js
+++ Time-Date/main.js
@@ -4,22 +4,9 @@ var n = d.getDay()
 var t = d.getHours()
 var m = d.getMinutes()
 var s = d.getSeconds()
-if(n==1){
-    n= "Monday";
-} else if(n==2) {
-    n= "Thursday";
-} else if(n==3) {
-    n= "Wednesday";
-} else if(n==4) {
-    n= "Tusday";
-} else if(n==5) {
-    n= "Friday";
-} else if(n==6) {
-    n= "Suturday";
-} else if(n==7) {
-    n= "Sunday"
-}
 
+let nn = ["zero","Monday","Thursday","Wednesday","Tusday","Friday","Suturday","Sunday"];
+let nnn = nn[n];
 
 let tt = t;
 if (tt > 12 && tt != 0) {
@@ -34,7 +21,7 @@ if (tt > 12 && tt != 0) {
     document.getElementById("time").innerHTML = "Current time is: " + t + " AM" + ": " + m + ": " + s;
 }
 
-document.getElementById("date").innerHTML = "Today is " + n;
+document.getElementById("date").innerHTML = "Today is " + nnn;
 
 
 
